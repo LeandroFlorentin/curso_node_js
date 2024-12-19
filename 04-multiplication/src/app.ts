fcf7071dd71fed1cc5941a330319bac6297ade1e
Leandro Florentin <102199433+LeandroFlorentin@users.noreply.github.com>
2024-12-18 23:45:18 -0300

    strating-clean-architecture

--- 04-multiplication/src/app.ts
+++ 04-multiplication/src/app.ts
@@ -1,9 +1,11 @@
 import { yarg } from "./config/plugins/yargs.plugins";
+import ServerApp from "./presentation/server-app";
 
 (() => {
   main();
 })();
 
 async function main() {
-  console.log(yarg);
+  const { b: base, l: limit, s: showTable } = yarg;
+  ServerApp.run({ base, limit, showTable });
 }
---
+++ 04-multiplication/src/domain/uses-cases/create-table.use-case.ts
@@ -0,0 +1,23 @@
+export interface CreateTableUseCase {
+  execute: (options: CreateTableOptions) => string;
+}
+
+export interface CreateTableOptions {
+  base: number;
+  limit?: number;
+}
+
+export class CreateTable implements CreateTableUseCase {
+  constructor() {
+    /*
+     *DI - Dependency Injection
+     */
+  }
+  execute({ base, limit = 10 }: CreateTableOptions) {
+    let text = "";
+    for (let i = 1; i < limit + 1; i++) {
+      text += `${base} x ${i} = ${base * i}\n`;
+    }
+    return text;
+  }
+}
