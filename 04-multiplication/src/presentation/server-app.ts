3cc9c5c1923080b7ce08234c4599cc100697c0a2
Leandro Florentin <102199433+LeandroFlorentin@users.noreply.github.com>
2024-12-26 23:04:58 -0300

    uses-cases

--- 04-multiplication/src/presentation/server-app.ts
+++ 04-multiplication/src/presentation/server-app.ts
@@ -1,4 +1,5 @@
 import { CreateTable } from "../domain/uses-cases/create-table.use-case";
+import { SaveFile } from "../domain/uses-cases/save-file.use-case";
 
 interface RunOptions {
   base: number;
@@ -10,6 +11,15 @@ export default class ServerApp {
   static run({ base, limit, showTable }: RunOptions) {
     console.log("Server running ...");
     const table = new CreateTable().execute({ base, limit });
+    try {
+      const wasCreated = new SaveFile().execute({
+        fileContent: table,
+        fileName: `outputs/table-${base}`,
+      });
+      console.log("File was created: ");
+    } catch (error) {
+      console.log("Error created file: ", error);
+    }
     if (showTable) console.log(table);
   }
 }
